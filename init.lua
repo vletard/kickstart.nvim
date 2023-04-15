28247022dc24b0002beb750db4d208ad13d96f26
Vincent Letard <letard.vincent@gmail.com>
2023-04-15 13:27:09 -0400

    Added Codeium dependency.

--- init.lua
+++ init.lua
@@ -95,6 +95,8 @@ require('lazy').setup({
     'hrsh7th/nvim-cmp',
     dependencies = { 'hrsh7th/cmp-nvim-lsp', 'L3MON4D3/LuaSnip', 'saadparwaiz1/cmp_luasnip' },
   },
+  -- Codeium completion
+  'Exafunction/codeium.vim',
 
   -- Useful plugin to show you pending keybinds.
   { 'folke/which-key.nvim', opts = {} },
