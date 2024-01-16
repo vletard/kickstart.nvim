990efcb83d15d079a69fb34bac9793e3bda30525
Vincent Letard <letard.vincent@gmail.com>
2024-01-15 22:42:09 -0500

    Added nvim-java

--- init.lua
+++ init.lua
@@ -188,6 +188,27 @@ require('lazy').setup({
   --    to get rid of the warning telling you that there are not plugins in `lua/custom/plugins/`.
   'yamatsum/nvim-cursorline',
   { import = 'custom.plugins' },
+  {
+    'nvim-java/nvim-java',
+    dependencies = {
+      'nvim-java/lua-async-await',
+      'nvim-java/nvim-java-core',
+      'nvim-java/nvim-java-test',
+      'nvim-java/nvim-java-dap',
+      'MunifTanjim/nui.nvim',
+      'neovim/nvim-lspconfig',
+      'mfussenegger/nvim-dap',
+      {
+        'williamboman/mason.nvim',
+        opts = {
+          registries = {
+            'github:nvim-java/mason-registry',
+            'github:mason-org/mason-registry',
+          },
+        },
+      }
+    },
+  },
 }, {})
 
 -- [[ Setting options ]]
